ee560d8c4e8930d879874230c92d7f974e0cab71
Daniel <juandaniel.25@outlook.com>
2023-04-22 18:57:44 -0500

    updated

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -43,7 +43,7 @@ const handler = NextAuth({
            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
          })
        ],
-       secret:process.env.JWT_SECRET,
+       secret:process.env.NEXTAUTH_SECRET,
        pages:{
         signIn:"/"
        }
